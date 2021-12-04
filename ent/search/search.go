01b2577a6ed01211135bc870fa5c8fb5cbc0da54
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-12-04 12:13:41 -0600

    update input/output formats

--- ent/search/search.go
+++ ent/search/search.go
@@ -4,20 +4,11 @@ import "github.com/gnames/gnlib/ent/verifier"
 
 type Output struct {
 	Meta  `json:"metadata"`
-	Names []Canonical `json:"names,omitempty"`
+	Names []*verifier.Name `json:"names,omitempty"`
 }
 
 type Meta struct {
-	Input    Input  `json:"input"`
-	NamesNum int    `json:"namesNumber"`
-	Error    string `json:"error,omitempty"`
-}
-
-type Canonical struct {
-	ID          string                 `json:"id"`
-	Name        string                 `json:"name"`
-	Cardinality int                    `json:"cardinality,omitempty"`
-	MatchType   string                 `json:"matchType"`
-	BestResult  *verifier.ResultData   `json:"bestResult,omitempty"`
-	Results     []*verifier.ResultData `json:"results,omitempty"`
+	Input       Input  `json:"input"`
+	NamesNumber int    `json:"namesNumber"`
+	Error       string `json:"error,omitempty"`
 }
